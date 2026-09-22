08e5be67f6496328719c46596b4a4c679621b133
Jeremy Hennessy <jeremy.hennessy@gmail.com>
2026-09-21 23:34:16 -0300

    Add quantum chemistry category styling

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
     ["Historical foundations","#f59e0b"],["Formulations","#60a5fa"],["Foundations & interpretations","#c084fc"],
     ["Quantum field theory","#34d399"],["Quantum information & open systems","#22d3ee"],["Quantum gravity & spacetime","#f472b6"],
     ["Quantum cosmology","#fb7185"],["Beyond standard quantum theory","#facc15"],["Mathematical structures","#a3e635"],
-    ["Quantum optics & AMO","#f0abfc"],["Quantum many-body & condensed matter","#2dd4bf"]
+    ["Quantum optics & AMO","#f0abfc"],["Quantum many-body & condensed matter","#2dd4bf"],["Quantum chemistry & electronic structure","#a7f3d0"]
   ]);
   const relationLabels = {
     precursor:"precursor of",reformulates:"reformulates",extends:"extends","challenged by":"challenges / challenged by",
